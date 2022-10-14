50d70dc6605d2ae6c7eb3306ae2aa46b2facc110
svtom <svtomusjak@gmail.com>
2022-10-14 11:53:12 +0300

    data new

---
+++ functions.php
@@ -0,0 +1,36 @@
+<?php
+add_filter( 'register_post_type_args', function( $args, $post_type ) {
+
+    // Change this to the post type you are adding support for
+    if ( 'product' === $post_type ) {
+        $args['show_in_graphql'] = true;
+        $args['graphql_single_name'] = 'product';
+        $args['graphql_plural_name'] = 'products';
+    }
+
+    return $args;
+
+}, 10, 2 );
+
+
+
+acf_add_options_page(array(
+    'page_title' 	=> 'Налаштування теми',
+    'menu_title'	=> 'Налаштування теми',
+    'menu_slug' 	=> 'theme-general-settings',
+    'capability'	=> 'edit_posts',
+    'redirect'		=> false,
+    'show_in_graphql' => true,
+    'graphql_field_name' => 'ThemeGeneralSettings',
+));
+
+add_theme_support( 'menus' );
+add_action( 'after_setup_theme', function(){
+    register_nav_menus( [
+        'HEADER_TOP' => 'Головне меню',
+        'FOOTER_1' => 'Футер меню 1',
+        'FOOTER_2' => 'Футер меню 2',
+        'FOOTER_3' => 'Футер меню 3',
+    ] );
+
+} );
